5a182ca26fd7710b875ad75e9c4627b06d8b38a9
marco201091-glitch <marco201091@gmail.com>
2026-06-03 09:02:27 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,3 +1,6 @@
 HTML = """
-Whatever you want
+We have bigger fish to fry.
+It really bugged me.
+I imagine you think of this activity as something characters from a Jane Austin novel did.
+Your stance tells me.
 """
